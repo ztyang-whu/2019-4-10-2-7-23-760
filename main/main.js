31048270d6d69870aa5559f8e0046fdf0de0d124
ztyang <2212349804@qq.com>
2019-05-31 22:10:32 +0800

    submit main.js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,13 @@
 module.exports = function main(inputs) {
     // write your code here...
-    return "price";
+    var result=6;
+    if(inputs.distance>2&&inputs.distance<=8){
+        result+=0.8*(inputs.distance-2);
+    }
+    if(inputs.distance>8){
+        result+=1.2*(inputs.distance-8)+0.8*6;
+    }
+    result+=inputs.parkTime*0.25;
+    result=Math.round(result);
+    return result;
 };
